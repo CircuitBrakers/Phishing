2bcda4ec928e855517ae5b9e9a24e74e6fd18b80
CircuitBrakers <97853935+CircuitBrakers@users.noreply.github.com>
2022-01-16 18:37:23 -0300

    Add files via upload

---
+++ Phishing/phishing generico/pages/sorteo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lottery | Page of Lottery</title>
+</head>
+<body>
+    <!-- La idea es hacer un formulario con correo(para informar que supuestamente perdio)    y direccion(para supestamente hacer llegar el premio) -->
+</body>
+</html>
